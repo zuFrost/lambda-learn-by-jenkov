35d651b0851bb076eafe46054d1e7ed0cedbba72
Alex Frost <zufrost.a@gmail.com>
2020-05-06 11:08:51 +0300

    5 примеров лямбд

--- scr/LearnMain.java
+++ scr/LearnMain.java
@@ -1,5 +1,10 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class LearnMain {
     public static void main(String[] args) {
@@ -20,5 +25,31 @@ public class LearnMain {
             System.out.println(x + " : " + y);
         };
 
+        List<Integer> list = Arrays.asList(1, 2, 3, 4 , 5, 6, 7, 8, 9);
+
+        System.out.println("output all elements ");
+        evaluate(list, (n)-> true);
+
+        System.out.println("не выводит ни одного числа: ");
+        evaluate(list, n -> false);
+
+        System.out.println("вывод четных чисел: ");
+        evaluate(list, n -> n % 2 == 0);
+
+        System.out.println("вывод нечетных чисел: ");
+        evaluate(list, n -> n % 2 == 1);
+
+        System.out.println("вывод чисел больше 5: ");
+        evaluate(list, n -> n > 5);
+
+    }
+
+    public static void evaluate(List<Integer> list, Predicate<Integer> predicate) {
+        for(Integer n: list) {
+            if (predicate.test(n))  {
+                System.out.print(n + " ");
+            }
+        }
+        System.out.println();
     }
 }
